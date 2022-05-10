f8782de981cf95e148e53f399b2fd9cef4cba9c3
Nicolas Tarulli <nicolastarulli@hotmail.com>
2022-05-10 03:00:00 -0300

    Simple conn with postgres, config set, simple insert to db

--- Main.go
+++ Main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	modelos "main/Models" // Importa el paquete modelos,
+	//la ruta tiene que ser el
+	//NombreDelModulo/folderDondeEstaElPaquete
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	dsn := "host=localhost port=5432 user=postgres dbname=ControlGastos password=1 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	nuevoUsuario := modelos.Persona{Nombre: "Juan", DNI: "12345678"}
+
+	if err != nil {
+		panic(err)
+	} else {
+		db.Create(&nuevoUsuario) //Se pasa la referencia de la estructura
+	}
+
+	print(nuevoUsuario.IdPersona)
 }
