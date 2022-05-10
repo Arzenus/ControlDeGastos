cf64b9ceaa73ec0f3c21354cd8253197d5f6135d
Nicolas Tarulli <nicolastarulli@hotmail.com>
2022-05-10 02:57:47 -0300

    Added Configuration file

---
+++ Configurations/Config.go
@@ -0,0 +1,23 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Host string `mapstructure:"POSTGRES_CONN"`
+}
+
+func LoadConfig() (config Config, err error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return config, err
+	}
+
+	err = viper.Unmarshal(&config)
+	return config, err
+}
